db20432be7c3c61334eb4f411bba08b22af9ab43
Sreeparvathy13 <sreeparvathydotwibe@outlook.com>
2026-07-12 13:13:19 +0530

    header up

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -18,80 +18,85 @@ const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <header>
-      {/* Desktop */}
-      <div className="header-container for-desk">
-        <div className="logo">
-          <Image src="/icons/logo.png" alt="Logo" width={40} height={40} />
-        </div>
+    <>
+      <header>
+        {/* Desktop */}
+        <div className="header-container for-desk">
+          <div className="logo">
+            <Image src="/icons/logo.png" alt="Logo" width={40} height={40} />
+          </div>
 
-        <nav>
-          <ul>
-            {navLinks.map((link, index) => (
-              <li key={index}>
-                <Link href={link.url}>{link.name}</Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
+          <nav>
+            <ul>
+              {navLinks.map((link, index) => (
+                <li key={index}>
+                  <Link href={link.url}>{link.name}</Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
 
-        <Link href="/" className="sign-inbtn">
-          Sign In
-          <svg
-  xmlns="http://www.w3.org/2000/svg"
-  width="14"
-  height="14"
-  viewBox="0 0 20 20"
-  fill="none"
->
-  <path
-    d="M5 15L15 5M15 5H8M15 5V12"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-  />
-</svg>
-        </Link>
-      </div>
-
-      {/* Mobile */}
-      <div className="header-container for-mob">
-        <div className="logo">
-          <Image src="/icons/logo.png" alt="Logo" width={40} height={40} />
-        </div>
-
-        <div className="mob-right">
           <Link href="/" className="sign-inbtn">
             Sign In
-
             <svg
-  xmlns="http://www.w3.org/2000/svg"
-  width="20"
-  height="20"
-  viewBox="0 0 20 20"
-  fill="none"
->
-  <path
-    d="M5 15L15 5M15 5H8M15 5V12"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-  />
-</svg>
-
-
+    xmlns="http://www.w3.org/2000/svg"
+    width="14"
+    height="14"
+    viewBox="0 0 20 20"
+    fill="none"
+  >
+    <path
+      d="M5 15L15 5M15 5H8M15 5V12"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
           </Link>
-
-          <button
-            className="hamburger"
-            onClick={() => setMenuOpen(true)}
-          >
-            ☰
-          </button>
         </div>
-      </div>
+
+        {/* Mobile */}
+        <div className="header-container for-mob">
+          <div className="logo">
+            <Image src="/icons/logo.png" alt="Logo" width={40} height={40} />
+          </div>
+
+          <div className="mob-right">
+            <Link href="/" className="sign-inbtn">
+              Sign In
+
+              <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+  >
+    <path
+      d="M5 15L15 5M15 5H8M15 5V12"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+
+
+            </Link>
+
+            <button
+              className="hamburger"
+              onClick={() => setMenuOpen(true)}
+            >
+
+
+                          <Image src="/icons/menu-burger.svg" alt="hamburget" width={30} height={30} />
+
+            </button>
+          </div>
+        </div>
+      </header>
 
       {/* Overlay */}
       <div className={`mobile-menu ${menuOpen ? "active" : ""}`}>
@@ -112,7 +117,7 @@ const Header = () => {
           ))}
         </ul>
       </div>
-    </header>
+    </>
   );
 };
 
